13076ef8093ecc1223857b99946b14b6d7c7b542
HolziHw <50407285+HolziHw@users.noreply.github.com>
2019-11-21 14:23:14 +0100

    Update kdjfhasdhfasdhfihasdf.cs

--- Datastructures/BinaryTree/models/kdjfhasdhfasdhfihasdf.cs
+++ Datastructures/BinaryTree/models/kdjfhasdhfasdhfihasdf.cs
@@ -7,4 +7,12 @@ namespace BinaryTree.models
     class kdjfhasdhfasdhfihasdf
     {
     }
+    
+    public void nichts()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            i = i + i;
+        }
+    }
 }
